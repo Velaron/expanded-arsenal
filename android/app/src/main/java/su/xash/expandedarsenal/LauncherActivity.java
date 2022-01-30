1dc80244399033a13cda15bc72dbdebdec6d17b6
Velaron <mars873@gmail.com>
2022-01-30 02:12:07 +0200

    it's insanely ugly, but it works, i'm tired

--- android/app/src/main/java/su/xash/expandedarsenal/LauncherActivity.java
+++ android/app/src/main/java/su/xash/expandedarsenal/LauncherActivity.java
@@ -1,32 +1,38 @@
 package su.xash.expandedarsenal;
 
 import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class LauncherActivity extends AppCompatActivity {
-	private static final int XASH_MIN_VERSION = 1710;
+	private static final int XASH_NEW_MIN_VERSION = 1710;
+	private static final int XASH_OLD_MIN_VERSION = 1200;
+	private static final int NEW_ENGINE = 0;
+	private static final int OLD_ENGINE = 1;
+	private static final int OLD_ENGINE_TEST = 2;
+
+	private static final String[] pkgs = {"su.xash.engine", "in.celest.xash3d.hl", "in.celest.xash3d.hl.test"};
+	private static final String[] components = {"su.xash.engine.XashActivity", "in.celest.xash3d.XashActivity", "in.celest.xash3d.XashActivity"};
 
 	@Override
 	public void onCreate(Bundle savedInstanceBundle) {
@@ -35,12 +41,26 @@ public class LauncherActivity extends AppCompatActivity {
 
 		ExtractAssets.extractPAK(this, false);
 
-		TextInputEditText launchParameters = (TextInputEditText) findViewById(R.id.launchParameters);
+		TextInputEditText launchParameters = findViewById(R.id.launchParameters);
 		launchParameters.setText("-log -dev 2");
 
-		ExtendedFloatingActionButton launchButton = (ExtendedFloatingActionButton) findViewById(R.id.launchButton);
+		ExtendedFloatingActionButton launchButton = findViewById(R.id.launchButton);
 		launchButton.setOnClickListener((view) -> {
-			startActivity(new Intent().setComponent(new ComponentName("su.xash.engine", "su.xash.engine.XashActivity"))
+			String pkg = pkgs[NEW_ENGINE], cls = components[NEW_ENGINE];
+			AutoCompleteTextView engineType = findViewById(R.id.engineType);
+
+			if (engineType.getText().toString().equals(getResources().getStringArray(R.array.engine_types)[NEW_ENGINE])) {
+				pkg = pkgs[NEW_ENGINE];
+				cls = components[NEW_ENGINE];
+			} else if (engineType.getText().toString().equals(getResources().getStringArray(R.array.engine_types)[OLD_ENGINE])) {
+				pkg = pkgs[OLD_ENGINE];
+				cls = components[OLD_ENGINE];
+			} else if (engineType.getText().toString().equals(getResources().getStringArray(R.array.engine_types)[OLD_ENGINE_TEST])) {
+				pkg = pkgs[OLD_ENGINE_TEST];
+				cls = components[OLD_ENGINE_TEST];
+			}
+
+			startActivity(new Intent().setComponent(new ComponentName(pkg, cls))
 					.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 					.putExtra("pakfile", getExternalFilesDir(null).getAbsolutePath() + "/extras.pak")
 					.putExtra("gamedir", "hl_expanded_arsenal")
@@ -50,60 +70,88 @@ public class LauncherActivity extends AppCompatActivity {
 
 		launchButton.setEnabled(false);
 
-		checkForEngine();
+		checkForEngines();
 	}
 
-	private void checkForEngine() {
+	private boolean checkForEngine(String packageName, int minVersion, String updateUrl) {
 		try {
-			PackageInfo info = getPackageManager().getPackageInfo("su.xash.engine", 0);
+			PackageInfo info = getPackageManager().getPackageInfo(packageName, 0);
 
-			if (info.versionCode < XASH_MIN_VERSION) {
-				openDialog("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous", getString(R.string.update_required),
-						getString(R.string.update_available, "Xash3D FWGS"));
+			if (info.versionCode < minVersion) {
+				openDialog(updateUrl, getString(R.string.update_required), getString(R.string.update_available, "Xash3D FWGS"));
 			} else {
-				checkForUpdates();
+				return true;
 			}
 		} catch (PackageManager.NameNotFoundException e) {
-			openDialog("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous",
-					getString(R.string.engine_not_found), getString(R.string.engine_info));
+			// openDialog(updateUrl, getString(R.string.engine_not_found), getString(R.string.engine_info));
+			return false;
+		}
+
+		return false;
+	}
+
+	private void checkForEngines() {
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item);
+
+		if (checkForEngine(pkgs[NEW_ENGINE], XASH_NEW_MIN_VERSION, "https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous")) {
+			arrayAdapter.add(getResources().getStringArray(R.array.engine_types)[NEW_ENGINE]);
+		}
+		if (checkForEngine(pkgs[OLD_ENGINE], XASH_OLD_MIN_VERSION, "https://github.com/FWGS/xash3d/releases/tag/v0.19.2")) {
+			arrayAdapter.add(getResources().getStringArray(R.array.engine_types)[OLD_ENGINE]);
+		}
+		if (checkForEngine(pkgs[OLD_ENGINE_TEST], 0, "https://github.com/FWGS/xash3d")) {
+			arrayAdapter.add(getResources().getStringArray(R.array.engine_types)[OLD_ENGINE_TEST]);
+		}
+
+		if (arrayAdapter.isEmpty()) {
+			new MaterialAlertDialogBuilder(LauncherActivity.this)
+					.setTitle(R.string.engine_not_installed)
+					.setMessage(R.string.should_install_engine)
+					.setCancelable(false)
+					.setNegativeButton(R.string.exit, (dialog, which) -> finish()).show();
+		} else {
+			AutoCompleteTextView engineType = findViewById(R.id.engineType);
+			engineType.setAdapter(arrayAdapter);
+			engineType.setText(arrayAdapter.getItem(0), false);
+
+			if (arrayAdapter.getCount() < 2) {
+				TextInputLayout engineTypeLayout = findViewById(R.id.engineTypeLayout);
+				engineTypeLayout.setEnabled(false);
+			}
+
+			checkForUpdates();
 		}
 	}
 
 	private void checkForUpdates() {
 		String url = "https://api.github.com/repos/Velaron/expanded-arsenal/commits/master";
-		CoordinatorLayout contextView = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
-		ExtendedFloatingActionButton launchButton = (ExtendedFloatingActionButton) findViewById(R.id.launchButton);
+		CoordinatorLayout contextView = findViewById(R.id.coordinatorLayout);
+		ExtendedFloatingActionButton launchButton = findViewById(R.id.launchButton);
 
 		Snackbar updateNotification = Snackbar.make(contextView, R.string.checking_for_updates, Snackbar.LENGTH_INDEFINITE).setAnchorView(launchButton);
 		updateNotification.show();
 
-		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-			@Override
-			public void onResponse(JSONObject response) {
-				try {
-					String sha = response.getString("sha").substring(0, 7);
-					String version_sha = getPackageManager().getPackageInfo(getPackageName(), 0).versionName.split("-")[1];
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+			try {
+				String sha = response.getString("sha").substring(0, 7);
+				String version_sha = getPackageManager().getPackageInfo(getPackageName(), 0).versionName.split("-")[1];
 
-					if (version_sha.equals(sha)) {
-						updateNotification.dismiss();
-						launchButton.setEnabled(true);
-					} else {
-						updateNotification.dismiss();
-						openDialog("https://github.com/Velaron/expanded-arsenal/releases/tag/continuous", getString(R.string.update_required),
-								getString(R.string.update_available, "Expanded Arsenal"));
-					}
-				} catch (JSONException e) {
-					e.printStackTrace();
-				} catch (PackageManager.NameNotFoundException e) {
-					e.printStackTrace();
+				if (version_sha.equals(sha)) {
+					updateNotification.dismiss();
+					launchButton.setEnabled(true);
+				} else {
+					updateNotification.dismiss();
+					openDialog("https://github.com/Velaron/expanded-arsenal/releases/tag/continuous", getString(R.string.update_required),
+							getString(R.string.update_available, "Expanded Arsenal"));
 				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (PackageManager.NameNotFoundException e) {
+				e.printStackTrace();
 			}
-		}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
-				updateNotification.dismiss();
-				launchButton.setEnabled(true);
-			}
+		}, error -> {
+			updateNotification.dismiss();
+			launchButton.setEnabled(true);
 		});
 
 		RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
@@ -115,17 +163,7 @@ public class LauncherActivity extends AppCompatActivity {
 				.setTitle(title)
 				.setMessage(description)
 				.setCancelable(false)
-				.setNegativeButton(R.string.exit, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						finish();
-					}
-				})
-				.setPositiveButton(R.string.update, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)));
-					}
-				}).show();
+				.setNegativeButton(R.string.exit, (dialog, which) -> finish())
+				.setPositiveButton(R.string.update, (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)))).show();
 	}
 }
